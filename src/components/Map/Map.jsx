8972464f68857ba98b19294c964d8b2f447e5755
Geoffrey Hanson <g.hanson.b@gmail.com>
2020-10-14 16:01:50 -0500

    Made Map component for embedded map

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Map.module.css';
+
+const Map = () => (
+  <iframe
+    title="Embedded Google Map"
+    src="https://www.google.com/maps/d/u/0/embed?mid=1Lzxsanw81e7VloBq1G5_1RZj9rGHrFck"
+  />
+);
+
+export default Map;
